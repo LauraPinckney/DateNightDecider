27ffe8a2c924739df614cc6c09a43d06ecd8b6a2
Laura Pinckney <laura.pinckney@yahoo.com>
2023-10-13 12:36:12 +0100

    nav bar skeleton added, sections created for cocktails and trivia, flex styling added to cocktail section

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,45 @@
-const myApiURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-const myApiKey = "&api_key=1"
+const myApiURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const myApiKey = '&api_key=1';
 
 const getCocktail = async () => {
-    try{
-        const response = await axios.get(`${myApiURL}?api_key=${myApiKey}`);
-        console.log(response.data);
-        const cocktail = response.data
-        createElements(cocktail)
-        return response.data;
-    } catch(error) {
-        console.log("error")
-    }
-}
-
-getCocktail()
-    
-function createElements(cocktail) {
-    const title = document.createElement("h2");
-    title.textContent = cocktail.drinks[0].strDrink;
-  
-    const image = document.createElement("img");
-    image.src = cocktail.drinks[0].strDrinkThumb;
-    image.alt = "Random Cocktail";
-    image.classList.add("img");
-  
-    const main = document.querySelector(".main");
-    main.appendChild(title);
-    main.appendChild(image);
-    
-    for (let i = 1; i < 16; i++){
-    console.log(i);
-    if(cocktail.drinks[0][`strIngredient${i}`] == null){
-        break;
-    }
-    
-    let ingredient = document.createElement('ons-list-item');
-    ingredient.classList.add("ingredients")
-    ingredient.innerText = cocktail.drinks[0][`strMeasure${i}`] + ':' + cocktail.drinks[0][`strIngredient${i}`]
-    main.appendChild(ingredient)
-    }
+  try {
+    const response = await axios.get(`${myApiURL}?api_key=${myApiKey}`);
+    console.log(response.data);
+    const cocktail = response.data;
+    createElements(cocktail);
+    return response.data;
+  } catch (error) {
+    console.log('error');
   }
-  
+};
+
+getCocktail();
+
+function createElements(cocktail) {
+  const title = document.createElement('h2');
+  title.textContent = cocktail.drinks[0].strDrink;
+
+  const image = document.createElement('img');
+  image.src = cocktail.drinks[0].strDrinkThumb;
+  image.alt = 'Random Cocktail';
+  image.classList.add('img');
+
+  const main = document.getElementById('cocktail-section');
+  main.appendChild(title);
+  main.appendChild(image);
+
+  for (let i = 1; i < 16; i++) {
+    console.log(i);
+    if (cocktail.drinks[0][`strIngredient${i}`] == null) {
+      break;
+    }
+
+    let ingredient = document.createElement('ons-list-item');
+    ingredient.classList.add('ingredients');
+    ingredient.innerText =
+      cocktail.drinks[0][`strMeasure${i}`] +
+      ':' +
+      cocktail.drinks[0][`strIngredient${i}`];
+    main.appendChild(ingredient);
+  }
+}
--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,40 +1,29 @@
 const myApiURLTrivia = 'http://jservice.io/api/random';
+const triviaObject = { question, answer };
 
-const getTrivia = async () => {
-  try {
-    const response = await axios.get(`${myApiURLTrivia}`);
-    const trivia = response.data;
-    createElements(trivia);
-    return response.data;
-  } catch (error) {
-    console.log('error');
-  }
-};
+const triviaDiv = document.createElement('div');
+triviaDiv.classList.add('trivia__container');
 
-getTrivia();
+const triviaAnswer = document.createElement('p');
+triviaAnswer.classList.add('trivia__answer');
+triviaAnswer.textContent = answer;
 
-// function createElements(trivia) {
-//   const title = document.createElement("h2");
-//   title.textContent = trivia.question;
+const triviaQuestion = document.createElement('p');
+triviaQuestion.classList.add('trivia__question');
+triviaQuestion.textContent = question;
 
-//   const image = document.createElement("img");
-//   image.src = cocktail.drinks[0].strDrinkThumb;
-//   image.alt = "Random Cocktail";
-//   image.classList.add("img");
+triviaDiv.appendChild(triviaAnswer);
+triviaDiv.appendChild(triviaQuestion);
 
-//   const main = document.querySelector(".main");
-//   main.appendChild(title);
-//   main.appendChild(image);
-
-//   for (let i = 1; i < 16; i++){
-//   console.log(i);
-//   if(cocktail.drinks[0][`strIngredient${i}`] == null){
-//       break;
+// const getTrivia = async () => {
+//   try {
+//     const response = await axios.get(`${myApiURLTrivia}`);
+//     const trivia = response.data;
+//     createElements(trivia);
+//     return response.data;
+//   } catch (error) {
+//     console.log('error');
 //   }
+// };
 
-//   let ingredient = document.createElement('ons-list-item');
-//   ingredient.classList.add("ingredients")
-//   ingredient.innerText = cocktail.drinks[0][`strMeasure${i}`] + ':' + cocktail.drinks[0][`strIngredient${i}`]
-//   main.appendChild(ingredient)
-//   }
-// }
+// getTrivia();
