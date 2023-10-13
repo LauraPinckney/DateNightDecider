9c755ee4d3046d0b9f5e9e4d54b358ff7eb2e3b7
Laura Pinckney <laura.pinckney@yahoo.com>
2023-10-13 17:45:49 +0100

    movies on home page and moved trivia

--- scripts/script.js
+++ scripts/script.js
@@ -57,15 +57,115 @@ const getTrivia = async () => {
     console.log('error');
   }
 };
+
 getTrivia();
+
 const triviaSec = document.getElementById('trivia-section');
 function createTriviaElement(trivia) {
   const answer = document.createElement('h2');
   answer.textContent = trivia[0].answer;
   triviaSec.appendChild(answer);
 }
+
 function createTriviaQuestion(trivia) {
   const question = document.createElement('h3');
   question.textContent = trivia[0].question;
   triviaSec.appendChild(question);
 }
+
+
+
+const movies = [
+  {
+    name: "Moana",
+    imgSrc: "./images/moana.jpg"
+  },
+  {
+    name: "The wolf of wall street",
+    img: "./images/wall-street.jpg"
+  },
+  {
+    name: "The commitments",
+    img: "./images/commitments.jpg"
+  },
+  {
+    name: "Top Gun",
+    img: "../images/top-gun.jpg"
+  },
+  {
+    name: "Spider-man",
+    img: "./images/spider.jpg"
+  },
+  {
+    name: "Guardians of the galaxy",
+    img: "./images/gotg.jpg"
+  },
+  {
+    name: "Shutter Island",
+    img: "./images/shutter-island.jpg"
+  },
+  {
+    name: "Paddington",
+    img: "./images/paddington.jpg"
+  },
+  {
+    name: "Titanic",
+    img: "./images/titanic.jpg"
+  },
+  {
+    name: "The Terminator",
+    img: "./images/terminator.jpg"
+  },
+  {
+    name: "Fast and Furous: Tokyo Drift",
+    img: "./images/faf.jpg"
+  },
+  {
+    name: "My Big Fat Greek Wedding",
+    img: "./images/greek-wedding.jpg"
+  },
+  {
+    name: "The Blindside",
+    img: "./images/blindside.jpg"
+  },
+  {
+    name: "Shaun Of The Dead",
+    img: "./images/shaun.jpg"
+  },
+  {
+    name: "Shrek",
+    img: "./images/shrek.jpg"
+  },
+  {
+    name: "Blade Runner 2049",
+    img: "./images/blade.jpg"
+  }
+]
+
+function addMovies() {
+  document.querySelector(".display__movie").innerText = "";
+      const movieCard = document.createElement("div");
+      const movieTitle = document.createElement("h2");
+      const movieImg = document.createElement("img");
+
+      movieTitle.classList.add("movie__title")
+      movieImg.classList.add("movie__image");
+
+       const randomMovie = getRandomNumber()
+      movieTitle.innerText = movies[randomMovie].name;
+      movieImg.setAttribute("src", movies[randomMovie].img)
+
+      movieCard.appendChild(movieTitle);
+      movieCard.appendChild(movieImg);
+
+
+      document.querySelector(".display__movie").appendChild(movieCard);
+    }
+
+
+  function getRandomNumber(){
+   return Math.floor(Math.random() * 15) 
+  }
+
+  getRandomNumber()
+  addMovies()
